b217675e3c06008ea45a0983e11076a485d86d50
kclamar <kclamar@connect.ust.hk>
2021-06-27 23:53:11 +0800

    Change pyproject.toml

--- stimpy/__init__.py
+++ stimpy/__init__.py
@@ -1,7 +1,8 @@
+import importlib.metadata
 from . import visual
 from .animate import Animate
 from .stim import Stimuli
 from .trial import Trial
 from .window import Window
 
-__version__ = "0.0.2"
+__version__ = importlib.metadata.version("stimpy")
