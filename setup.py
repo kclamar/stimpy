1b0954b3c44fec73c691bb84cead267eb1f865d9
kclamar <kclamar@connect.ust.hk>
2021-06-27 20:49:41 +0800

    Packaging

--- setup.py
+++ setup.py
@@ -1,60 +1,43 @@
-import codecs
-import os.path
+from setuptools import setup
 
-from setuptools import find_packages, setup
+packages = ["stimpy", "stimpy.animate"]
 
+package_data = {"": ["*"]}
 
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), "r") as fp:
-        return fp.read()
+install_requires = ["PsychoPy>=2021.2.0,<2022.0.0", "numpy>=1.21.0,<2.0.0"]
 
-
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith("__version__"):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
-
+extras_require = {
+    "dev": [
+        "black",
+        "isort",
+        "flake8",
+        "mypy",
+        "pydata-sphinx-theme",
+        "sphinx",
+        "sphinx-autodoc-typehints",
+    ]
+}
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(
-    name="stimpy",
-    version=get_version("pyproject.toml"),
-    packages=find_packages(),
-    url="https://github.com/kclamar/stimpy",
-    author="Ka Chung Lam",
-    author_email="kclamar@connect.ust.hk",
-    description="A PsychoPy wrapper to create moving visual stimuli without "
-    "loops.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: Microsoft :: Windows",
-        "Intended Audience :: Science/Research",
-    ],
-    install_requires=[
-        "numpy",
-        "psychopy",
-    ],
-    extras_require={
-        "dev": [
-            "black",
-            "isort",
-            "flake8",
-            "mypy",
-            "pydata-sphinx-theme",
-            "sphinx",
-            "sphinx-autodoc-typehints",
-        ]
-    },
-    python_requires=">=3.8",
-)
+setup_kwargs = {
+    "name": "stimpy",
+    "version": "0.1.0",
+    "description": "A PsychoPy wrapper to create moving visual stimuli without"
+    " loops.",
+    "long_description": long_description,
+    "author": "Ka Chung Lam",
+    "author_email": "kclamar@connect.ust.hk",
+    "maintainer": None,
+    "maintainer_email": None,
+    "url": "https://github.com/kclamar/stimpy",
+    "packages": packages,
+    "package_data": package_data,
+    "install_requires": install_requires,
+    "extras_require": extras_require,
+    "python_requires": ">=3.8,<4.0",
+}
+
+
+setup(**setup_kwargs)
